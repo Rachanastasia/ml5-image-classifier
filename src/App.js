62d369ef965023d3857dfcbc187546d7ae522fa2
Rachel Reilly <r.a.reill18@gmail.com>
2020-11-15 18:43:06 -0600

    added styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import * as ml5 from 'ml5';
+import './App.css';
+import './variables.css'
 
 function App() {
 
@@ -34,22 +36,29 @@ function App() {
 
 
   return (
-    <div className="App">
-      <h1>{ml5.version}</h1>
-      <input type='file' accept='image/*' onChange={loadImage} />
-      {
-        imageUrl &&
-        <div>
-          <img id='image' src={imageUrl} alt='to be classified' height={500} />
+    <main className="App">
+      <header>
+        <h1>Image Classifier</h1>
+      </header>
+      <section>
+        <div className='input_container'>
+          <input type='file' accept='image/*' onChange={loadImage} className='button' />
           <button onClick={classifyImage}>Classify</button>
         </div>
+        {
+          predicted &&
+          <p>The app is {predictionConfidence * 100}% sure that this is {predictionLabel.split(",")[0]} </p>
+        }
+        {
+          imageUrl &&
+          <img id='image' src={imageUrl} alt='to be classified' height={500} />
 
-      }
-      {
-        predicted &&
-        <p>The app is {predictionConfidence * 100}% sure that this is {predictionLabel.split(",")[0]} </p>
-      }
-    </div>
+
+
+        }
+
+      </section>
+    </main>
   );
 }
 
