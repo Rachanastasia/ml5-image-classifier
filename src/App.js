83ac233fb256ca6792d54b86e4e31cda573e7cbf
Rachel Reilly <r.a.reill18@gmail.com>
2020-11-15 18:24:16 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import * as ml5 from 'ml5';
 
 function App() {
+
+  const [ready, setReady] = useState(false)
+  const [predictionLabel, setPredictionLabel] = useState('')
+  const [predictionConfidence, setPredictionConfidence] = useState('')
+  const [predicted, setPredicted] = useState(false)
+  const [imageUrl, setImageUrl] = useState('')
+
+  const loadImage = (event) => {
+    const image = event.target.files[0]
+    setImageUrl(window.URL.createObjectURL(image))
+
+    console.log(image)
+  }
+
+
+  const classifyImage = async () => {
+    const classifier = await ml5.imageClassifier('MobileNet')
+    setReady(true)
+
+    const image = document.getElementById("image")
+
+    classifier.predict(image, 5, (err, results) => {
+      setPredictionLabel(results[0].label)
+      setPredictionConfidence(results[0].confidence)
+      setPredicted(true)
+    })
+  }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{ml5.version}</h1>
+      <input type='file' accept='image/*' onChange={loadImage} />
+      {
+        imageUrl &&
+        <div>
+          <img id='image' src={imageUrl} alt='to be classified' height={500} />
+          <button onClick={classifyImage}>Classify</button>
+        </div>
+
+      }
+      {
+        predicted &&
+        <p>The app is {predictionConfidence * 100}% sure that this is {predictionLabel.split(",")[0]} </p>
+      }
     </div>
   );
 }
